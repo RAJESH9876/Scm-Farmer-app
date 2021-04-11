264ed55038e87dddc3596f0a8a1b35831ef73e9e
Rajesh <grajeshkumar54321@gmail.com>
2021-04-11 14:30:08 +0530

    added-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BidderComponent } from './bidder/bidder.component';
+import { ViewMarketplaceComponent } from './view-marketplace/view-marketplace.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BidderMarketPlaceComponent } from './bidder-market-place/bidder-market-place.component';
+import { BidderWelcomeComponent } from './bidder-welcome/bidder-welcome.component';
+import { FarmerWelcomeComponent } from './farmer-welcome/farmer-welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    BidderComponent,
+    ViewMarketplaceComponent,
+    RegistrationComponent,
+    BidderMarketPlaceComponent,
+    BidderWelcomeComponent,
+    FarmerWelcomeComponent
   ],
   imports: [
     BrowserModule,
